4d3bb0157b3a035a0feccf7a9e793444c5dedea4
Taiye Ajimati <taiyeajimati@gmail.com>
2022-11-06 19:03:48 +0400

    Last office Commit

--- src/components/nav/SideNav.jsx
+++ src/components/nav/SideNav.jsx
@@ -6,12 +6,12 @@ import { Link } from 'react-scroll';
 
 export default function sideNav() {
   return (
-    <nav className='bg-primary text-[white] w-[80vw] lg:w-[100vw]  '>
+    <nav className='bg-primary text-[white] w-[80vw] lg:w-[100vw] py-[10rem]  '>
     <div className="logo"></div>
-      <ul className='flex flex-col items-start  space-y-6'>
+      <ul className='flex flex-col  items-start pl-10 space-y-6'>
         {links.map((item,id)=>(
     //<li className='flex flex-row  gap-4 border'><a href="#"><div><img className='inline' src={item.icon} alt="" /></div>{item.title}</a></li>
-    <li className=' w-[100%]  '><Link to={item.anchor} className='flex space-x-6 ml-10  cursor-pointer'><img className='inline' src={item.icon} alt="" /> <p className='text-sm uppercase lg:text-lg'>{item.title}</p></Link></li>
+    <li className='pr border   '><Link to={item.anchor} className='flex bg-green gap-6 cursor-pointer'><img className='inline' src={item.icon} alt="" /> <p className='text-sm uppercase lg:text-lg'>{item.title}</p></Link></li>
     
         ))}
       
