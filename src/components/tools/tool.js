4543343594f2a8c3ed53b244f5e3d62087db0eb7
Taiye Ajimati <taiyeajimati@gmail.com>
2022-11-08 18:23:19 +0400

    Last office Commit

---
+++ src/components/tools/tool.js
@@ -0,0 +1,46 @@
+import html from '../../img/html.png';
+import css from '../../img/css.png';
+import scss from '../../img/sass.png';
+import js from '../../img/js.png';
+import tailwind from '../../img/Tailwind.png';
+import react from '../../img/reactjs.png';
+import adobe from '../../img/xd.png';
+import figma from '../../img/figma.png';
+import wordpress from '../../img/wordpress.png';
+import git from '../../img/github.png';
+
+
+const tool=[
+    {
+        id:1,
+        icon:'html',
+        name:'HTML',
+    },
+    {
+        id:2,
+        icon:'css',
+        name:'CSS',
+    },
+    {
+        id:3,
+        icon:'scss',
+        name:'SCSS',
+    },
+    {
+        id:4,
+        icon:'js',
+        name:'Javascript',
+    },
+// 'React',
+// 'Vanila Js',
+// 'Html',
+// 'Vanila CSS',
+// 'TailwindCSS',
+// 'SCSS / SASS',
+// 'Figma',
+// 'Adobe Xd',
+// 'Github',
+// 'WordPress'
+]
+
+export default tool;
