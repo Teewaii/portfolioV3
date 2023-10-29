19a59b1f7516da05956dde52dba40509d345779e
teewai <taiyeajimati@gmail.com>
2023-10-29 12:50:22 +0000

    edex description

--- src/components/projects/project.js
+++ src/components/projects/project.js
@@ -1,132 +1,113 @@
 // import sass from '../../img/sass.jpg';
 
-import easy from '../../img/easy.jpg';
-import sabi_mockup from '../../img/sabi-01.jpg';
-import crowd from '../../img/crowdfunding.jpg';
-import restCountry from '../../img/restCountry.jpg';
-import time_tracking from '../../img/time-tracking.jpg';
-import freelance from '../../img/freelance.jpg';
-import sunnyside from '../../img/sunnyside.jpg';
-import edex from '../../img/edex_mock.jpg';
-
+import easy from "../../img/easy.jpg";
+import sabi_mockup from "../../img/sabi-01.jpg";
+import crowd from "../../img/crowdfunding.jpg";
+import restCountry from "../../img/restCountry.jpg";
+import time_tracking from "../../img/time-tracking.jpg";
+import freelance from "../../img/freelance.jpg";
+import sunnyside from "../../img/sunnyside.jpg";
+import edex from "../../img/edex_mock.jpg";
 
 const Images = [
-    {
-        id: 1,
-        title: "Rest Country APi ",
-        head: "Rest Country APi",
-        desc: "This is a practice project from frontend mentor. The aim of this challenge is to integrate the REST Countries API to pull country data and display it. Users should be able to filter the countries with different parameters.",
-        pix: restCountry,
-        git: 'https://github.com/Teewaii/restCountries.git',
-        live: 'https://rest-countries-weld.vercel.app/',
-        tools: [
-            "React.js", "TailwindCSS"
-        ]
-    },
-    {
-        id: 2,
-        title: "e-learning",
-        head: "e-learning website landing page",
-        desc: "A personal project intended for e-learning services. ",
-        pix: edex,
-        git: 'https://github.com/Teewaii/edex.git',
-        live: 'https://teewaii.github.io/edex/',
-        tools: [
-            "React.js", "TailwindCSS"
-        ]
-    },
-    {
+  {
+    id: 1,
+    title: "Rest Country APi ",
+    head: "Rest Country APi",
+    desc: "This is a practice project from frontend mentor. The aim of this challenge is to integrate the REST Countries API to pull country data and display it. Users should be able to filter the countries with different parameters.",
+    pix: restCountry,
+    git: "https://github.com/Teewaii/restCountries.git",
+    live: "https://rest-countries-weld.vercel.app/",
+    tools: ["React.js", "TailwindCSS"],
+  },
+  {
+    id: 2,
+    title: "e-learning",
+    head: "e-learning website landing page",
+    desc: "Dive into the world of online education with my E-Learning Hub Landing Page, a captivating showcase of the power of React and the elegance of Tailwind CSS. This project is the perfect gateway to a universe of knowledge and self-improvement.",
+    pix: edex,
+    git: "https://github.com/Teewaii/edex.git",
+    live: "https://teewaii.github.io/edex/",
+    tools: ["React.js", "TailwindCSS"],
+  },
+  {
+    id: 3,
+    title: "Hire Freelancer ",
+    head: "Hire Freelancer Landing Page ",
+    desc: "This is a demo website for hiring freelancers. The project was built for the purpose of having a feel of Next js.",
+    pix: freelance,
+    git: "https://github.com/Teewaii/hirefreelance.git",
+    live: "https://hirefreelance.vercel.app/",
+    tools: ["React.js", "Next js", "TailwindCSS"],
+  },
+  {
+    id: 4,
+    title: "Construction Single Page",
+    head: "Construction Single Page Website ",
+    desc: "A one pager personal project intended for a construction company that showcases the company's products and services. ",
+    pix: sabi_mockup,
+    git: "https://github.com/Teewaii/sabi-construction.git",
+    live: "https://teewaii.github.io/sabi-construction/",
+    tools: ["React.js", "TailwindCSS"],
+  },
 
-        id: 3,
-        title: "Hire Freelancer ",
-        head: "Hire Freelancer Landing Page ",
-        desc: "This is a demo website for hiring freelance. The project was built for the purpose of having a feel of Next js.",
-        pix: freelance,
-        git: 'https://github.com/Teewaii/hirefreelance.git',
-        live: 'https://hirefreelance.vercel.app/',
-        tools: [
-            "React.js", "Next js", "TailwindCSS"
-        ]
-    },
-    {
-        id: 4,
-        title: "Construction Single Page",
-        head: "Construction Single Page Website ",
-        desc: "A personal project intended for a construction company that showcases the company's products and services. ",
-        pix: sabi_mockup,
-        git: 'https://github.com/Teewaii/sabi-construction.git',
-        live: 'https://teewaii.github.io/sabi-construction/',
-        tools: [
-            "React.js", "TailwindCSS"
-        ]
-    },
+  {
+    id: 5,
+    title: "Easybank LP ",
+    head: "Easybank Landing Page",
+    desc: "This a bank demo front-end development challenge from Frontendmentor for personal development. ",
+    pix: easy,
+    git: "https://github.com/Teewaii/easybank-landing-page.git",
+    live: "https://teewaii.github.io/easybank-landing-page",
+    tools: ["React.js", "TailwindCSS"],
+  },
+  //   {
+  //     id: 6,
+  //     title: "Static Landing Page",
+  //     head: "Static Landing Page",
+  //     desc: "This a single static page project that I decided to play around with while tring to master CSS grid and I also love the UI layout and color contrast of the UI design. It's nothing but a static landing page and the design was gottent from frontendmentor.io",
+  //     pix: sunnyside,
+  //     git: "https://github.com/Teewaii/Sunnyside-agency-landing-page-repo.git",
+  //     live: "https://teewaii.github.io/Sunnyside-agency-landing-page-repo/",
+  //     tools: ["CSS", "Javascript"],
+  //   },
 
-    {
-        id: 5,
-        title: "Easybank LP ",
-        head: "Easybank Landing Page",
-        desc: "This a bank demo front-end development challenge from Frontendmentor for personal development. ",
-        pix: easy,
-        git: 'https://github.com/Teewaii/easybank-landing-page.git',
-        live: 'https://teewaii.github.io/easybank-landing-page',
-        tools: [
-            "React.js", "TailwindCSS"
-        ]
-    },
-    {
-        id: 6,
-        title: "Static Landing Page",
-        head: "Static Landing Page",
-        desc: "This a single static page project that I decided to play around with while tring to master CSS grid and I also love the UI layout and color contrast of the UI design. It's nothing but a static landing page and the design was gottent from frontendmentor.io",
-        pix: sunnyside,
-        git: 'https://github.com/Teewaii/Sunnyside-agency-landing-page-repo.git',
-        live: 'https://teewaii.github.io/Sunnyside-agency-landing-page-repo/',
-        tools: [
-            "CSS", "Javascript"
-        ]
-    },
+  {
+    id: 7,
+    title: "Time tracking ",
+    head: "Time tracking",
+    desc: "This a Time tracking demo front-end development challenge from Frontendmentor for personal development. Users will be able to View the optimal layout for the site depending on their device's screen size, See hover states for all interactive elements on the page, Switch between viewing Daily, Weekly, and Monthly stats. ",
+    pix: time_tracking,
+    git: "https://github.com/Teewaii/Time-tracking-dashboard-repo.git",
+    live: " https://teewaii.github.io/Time-tracking-dashboard-repo/",
+    tools: ["CSS", "Javascript"],
+  },
 
+  // {
+  //     id: 7,
+  //     title: "Crowdfunding ",
+  //     head: "Crowdfunding Landing Page",
+  //     desc: "This a crowd funding demo front-end development challenge from Frontendmentor for personal development. Users will be able make a selection of which pledge to make.",
+  //     pix: crowd,
+  //     git: 'https://github.com/Teewaii/crowdfunding-product-page.git',
+  //     live: 'https://teewaii.github.io/crowdfunding-product-page/',
+  //     tools: [
+  //         "React.js", "SCSS"
+  //     ]
+  // },
 
-    {
-        id: 7,
-        title: "Time tracking ",
-        head: "Time tracking",
-        desc: "This a Time tracking demo front-end development challenge from Frontendmentor for personal development. Users will be able to View the optimal layout for the site depending on their device's screen size, See hover states for all interactive elements on the page, Switch between viewing Daily, Weekly, and Monthly stats. ",
-        pix: time_tracking,
-        git: 'https://github.com/Teewaii/Time-tracking-dashboard-repo.git',
-        live: ' https://teewaii.github.io/Time-tracking-dashboard-repo/',
-        tools: [
-            "CSS", "Javascript"
-        ]
-    },
-
-    // {
-    //     id: 7,
-    //     title: "Crowdfunding ",
-    //     head: "Crowdfunding Landing Page",
-    //     desc: "This a crowd funding demo front-end development challenge from Frontendmentor for personal development. Users will be able make a selection of which pledge to make.",
-    //     pix: crowd,
-    //     git: 'https://github.com/Teewaii/crowdfunding-product-page.git',
-    //     live: 'https://teewaii.github.io/crowdfunding-product-page/',
-    //     tools: [
-    //         "React.js", "SCSS"
-    //     ]
-    // },
-
-
-    // {
-    //     id: 3,
-    //     title: "Sass Landing Page ",
-    //     head: "Digital Marketing Landing Page ",
-    //     desc: "A personal project intended for a digital marketing company that showcases the company's products and services. ",
-    //     pix: sass,
-    //     git: 'https://github.com/Teewaii/Digital-Marketing.git',
-    //     live: 'https://teewaii.github.io/Digital-Marketing',
-    //     tools: [
-    //         "React.js", "TailwindCSS"
-    //     ]
-    // },
-
-]
+  // {
+  //     id: 3,
+  //     title: "Sass Landing Page ",
+  //     head: "Digital Marketing Landing Page ",
+  //     desc: "A personal project intended for a digital marketing company that showcases the company's products and services. ",
+  //     pix: sass,
+  //     git: 'https://github.com/Teewaii/Digital-Marketing.git',
+  //     live: 'https://teewaii.github.io/Digital-Marketing',
+  //     tools: [
+  //         "React.js", "TailwindCSS"
+  //     ]
+  // },
+];
 
 export default Images;
